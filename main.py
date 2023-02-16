bd9be37b74b7ea15e587d0cd38b0c780cb2068b0
aubrie0297 <124844398+aubriepiper@users.noreply.github.com>
2023-02-16 00:45:06 +0000

    I changed variable name to company_name

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to my program!")
+company_name = input("Please enter your company name: ")
+feet = input("Please enter the number of feet of cable: ")
+feet = float(feet)
+cost = feet * .87
+cost = str(cost)
+print("The total cost for " + company_name + " is: $" + cost)
